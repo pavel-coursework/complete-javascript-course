47c111c53f16d0065621eb7f1ad52f6657ff65c3
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-22 19:09:49 +0200

    Add "values and variables" code

---
+++ 01-Fundamentals-Part-1/02-values-and-variables.js
@@ -0,0 +1,22 @@
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+let PI = 3.1415;
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+let job1 = "programmer";
+let job2 = "teacher";
+console.log(myFirstJob);
+
+// Variable naming convention
+// 1- Variables name should not start with a number let 5name = "George" is going to through a syntax error
+
+// 2- Variable names can contain letters, numbers, underscores or $ (dollar) sign if I try to declare a variable name first&name is going to through an error on the console
+
+// 3- Variable names should not be same to reserved language words for example <i>new</i> or <i>function</i>.
+
+// 4- Variable names should not start with uppercase letter let Person, usually this syntax is being used for classes in JS
+
+// 5- Constants should be written in uppercase, so it is easy to understand that the value store is not going to change. let PI = 3.14
+
+// 6- Variables name should be descriptive
