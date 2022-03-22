f25b29e67ccdf1e2434d8743b8d20f33a043a3f6
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-22 19:32:48 +0200

    Add "operator precedence" code

---
+++ 01-Fundamentals-Part-1/06-operator-precedence.js
@@ -0,0 +1,13 @@
+// Operator precedence is a collection of rules in JavaScript that reflect conventions about which procedure to perform first in a given mathematical expression.
+
+// MDN → https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
