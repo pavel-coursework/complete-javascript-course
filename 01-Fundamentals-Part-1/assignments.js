ea8141fce5c66ee25b6e908c359c5cab1da76844
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-22 19:29:16 +0200

    Add "let, const and var" assignment

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -26,3 +26,14 @@ console.log(typeof isIsland); // boolean
 console.log(typeof population); // number
 console.log(typeof country); // string
 console.log(typeof language); // undefined
+
+
+/* LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens */
+
+language = "Bulgarian";
+// isIsland = true (Uncaught TypeError: Assignment to constant variable.)
