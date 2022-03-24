eb8bf69dc5c900b0066669a0d6c7e96509f68885
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-24 07:53:35 +0200

    Add "logical operators" assignment

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -138,3 +138,23 @@ if (numNeighbours === 1) {
 } else {
   console.log("No borders")
 }
+
+
+/* LECTURE: Logical Operators
+1. Comment out the previous code so the prompt doesn't get in the way
+2. Let's say Sarah is looking for a new country to live in. She wants to live in a
+country that speaks english, has less than 50 million people and is not an
+island
+3. Write an if statement to help Sarah figure out if your country is right for her.
+You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this
+4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If
+not, log 'Portugal does not meet your criteria :('
+5. Probably your country does not meet all the criteria. So go back and temporarily
+change some variables in order to make the condition true (unless you live in
+Canada :D) */
+
+if (language === "English" && population < 50 && !isIsland) {
+  console.log(`Your should live in ${country} :)`);
+} else {
+  console.log(`${country} does not meet your criteria :()`);
+}
