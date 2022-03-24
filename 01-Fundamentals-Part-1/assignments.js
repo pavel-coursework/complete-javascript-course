bb99eb52dc492d0ee69b0aa32ff1cdb82392e407
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-24 07:42:57 +0200

    Add "type conversion and coercion" assignment

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -91,3 +91,19 @@ if (population > 33) {
   console.log(`${country}'s population is ${33 - population} million below average`);
   // → Bulgaria's population is 25,2 million below average
 }
+
+
+/* LECTURE: Type Conversion and Coercion
+1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+2. Execute the operations to check if you were right */
+
+console.log('9' - '5'); // → 4
+console.log('19' - '13' + '17'); // → "617"
+console.log('19' - '13' + 17); // → 23
+console.log('123' < 57); // → false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // → 1143
