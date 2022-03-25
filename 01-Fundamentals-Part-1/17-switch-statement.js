dbc4271b2e285b0b25e5a0f8960d033bdffe57d1
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-25 08:24:44 +0200

    Add "switch statement" code

---
+++ 01-Fundamentals-Part-1/17-switch-statement.js
@@ -0,0 +1,42 @@
+// Switch statement
+const day = "thursday";
+
+// • Switch statement does strict comparison ===
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday": // • We can have more than one true cases
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid date!");
+}
+
+// Replace the code above with an if/else statement
+if (day === "monday") {
+  console.log("Plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid date!");
+}
