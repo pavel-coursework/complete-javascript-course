d1a0c0db6dee63793ea0b42ac8a6adaecef368e7
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-26 16:36:09 +0200

    Add "function declarations vs expressions" assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -20,3 +20,33 @@ console.log(descJapan);
 // → Japan has 125.8 million people and its capital city is Tokyo
 console.log(descChina);
 // → China has 1449 million people and its capital city is Beijing
+
+
+/* LECTURE: Function Declarations vs. Expressions
+1. The world population is 7900 million people. Create a function declaration
+called 'percentageOfWorld1' which receives a 'population' value, and
+returns the percentage of the world population that the given population
+represents. For example, China has 1441 million people, so it's about 18.2% of
+the world population
+2. To calculate the percentage, divide the given 'population' value by 7900
+and then multiply by 100
+3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+store the results into variables, and log them to the console
+4. Create a function expression which does the exact same thing, called
+'percentageOfWorld2', and also call it with 3 country populations (can be
+the same populations) */
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const percBulgaria1 = percentageOfWorld1(6.8);
+console.log(percBulgaria1); // → 0.08607594936708861
+const percJapan1 = percentageOfWorld1(125.8);
+console.log(percJapan1); // → 1.5924050632911393
+const percChina2 = percentageOfWorld2(1449);
+console.log(percChina2); // → 18.341772151898734
