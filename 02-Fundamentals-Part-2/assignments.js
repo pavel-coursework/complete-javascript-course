ea0f8fb440c0fd65fa3a1ce3cc5140b8d1565a26
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-27 20:55:48 +0300

    Add "basic array operations" assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -105,3 +105,36 @@ const percentages = [
 ];
 console.log(percentages);
 // → [0.08607594936708861, 18.240506329113924, 1.5822784810126582, 1.0506329113924051]
+
+
+/* LECTURE: Basic Array Operations (Methods)
+1. Create an array containing all the neighbouring countries of a country of your
+choice. Choose a country which has at least 2 or 3 neighbours. Store the array
+into a variable called 'neighbours'
+2. At some point, a new country called 'Utopia' is created in the neighbourhood of
+your selected country. So add it to the end of the 'neighbours' array
+3. Unfortunately, after some time, the new country is dissolved. So remove it from
+the end of the array
+4. If the 'neighbours' array does not include the country ‘Germany’, log to the
+console: 'Probably not a central European country :D'
+5. Change the name of one of your neighbouring countries. To do that, find the
+index of the country in the 'neighbours' array, and then use that index to
+change the array at that index position. For example, you can search for
+'Sweden' in the array, and then replace it with 'Republic of Sweden' */
+
+const neighbours = ["Romania", "Serbia", "Macedonia", "Greece", "Turkey"];
+console.log(neighbours); // → ['Romania', 'Serbia', 'Macedonia', 'Greece', 'Turkey']
+
+neighbours.push("Utopia");
+console.log(neighbours); // → ['Romania', 'Serbia', 'Macedonia', 'Greece', 'Turkey', 'Utopia']
+
+neighbours.pop();
+console.log(neighbours); // → ['Romania', 'Serbia', 'Macedonia', 'Greece', 'Turkey']
+
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central European country :D");
+  // → Probably not a central European country :D
+}
+
+neighbours[neighbours.indexOf("Macedonia")] = "Kosovo";
+console.log(neighbours); // → ['Romania', 'Serbia', 'Kosovo', 'Greece', 'Turkey']
