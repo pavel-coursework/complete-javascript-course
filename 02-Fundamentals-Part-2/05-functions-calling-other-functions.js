21a1416b9990a5907e8b9327c16f1629be0c7856
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-27 17:45:47 +0300

    Update "functions calling other functions" code

---
+++ 02-Fundamentals-Part-2/05-functions-calling-other-functions.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+// → Juice with 8 pieces of apples and 12 pieces of oranges.
