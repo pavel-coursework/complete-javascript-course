c98868ff83295a73b446c1db6288fece25fe82e0
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-28 14:46:29 +0300

    Add "introduction to objects" code

---
+++ 02-Fundamentals-Part-2/10-introduction-to-objects.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// In array the order of elements matter because we access them using their order
+const jonasArray = [
+  "Jonas",
+  "Schmedtmann",
+  2022 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+];
+
+//Object jonas has 5 property and the order of the property does not matter when we retrieve them
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2022 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
