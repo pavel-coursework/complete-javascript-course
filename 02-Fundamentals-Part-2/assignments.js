6e61c230816f5218437f63ee5724f787d9efcc3c
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-28 14:47:28 +0300

    Add "introduction to objects" assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -138,3 +138,17 @@ if (!neighbours.includes("Germany")) {
 
 neighbours[neighbours.indexOf("Macedonia")] = "Kosovo";
 console.log(neighbours); // → ['Romania', 'Serbia', 'Kosovo', 'Greece', 'Turkey']
+
+
+/* LECTURE: Introduction to Objects
+1. Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and
+'neighbours' (an array like we used in previous assignments) */
+
+const myCountry = {
+  country: "Bulgaria",
+  capital: "Sofia",
+  language: "Bulgarian",
+  population: 6.8,
+  neighbours: ["Romania", "Serbia", "Macedonia", "Greece", "Turkey"]
+};
