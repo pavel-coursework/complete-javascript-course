6442865d2f3349b12562fc16a7ef7ca138bad1e4
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-29 08:58:38 +0300

    Add "while loop" code

---
+++ 02-Fundamentals-Part-2/17-while-loop.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log(`Loop is about to end..`);
+}
