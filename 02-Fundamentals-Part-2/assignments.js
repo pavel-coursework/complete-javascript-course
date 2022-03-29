2c16fb64f14e6cbfbace7cc07589c7cb7667d4e2
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-29 08:53:04 +0300

    Add "for loop" assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -201,3 +201,13 @@ myCountry.checkIsland = function () {
   return this.isIsland;
 }
 console.log(myCountry.checkIsland()); // → false
+
+
+/* LECTURE: Iteration: The for Loop
+1. There are elections in your country! In a small town, there are only 50 voters.
+Use a for loop to simulate the 50 people voting, by logging a string like this to
+the console (for numbers 1 to 50): 'Voter number 1 is currently voting' */
+
+for (let i = 1; i <= 50; i++) {
+  console.log(`Voter number ${i} is currently voting`);
+}
