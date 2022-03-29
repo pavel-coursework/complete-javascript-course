1046d7ed79424b6f8dd522748d2522aedb10e727
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-29 08:59:53 +0300

    Add coding challenge 4

---
+++ 02-Fundamentals-Part-2/18-coding-challenge-4.js
@@ -0,0 +1,49 @@
+/* Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for
+loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  const total = bills[i] + tip;
+
+  tips.push(tip);
+  totals.push(total);
+}
+
+console.log(tips);
+// → [4.4, 44.25, 26.4, 88, 7.4, 15.75, 2, 220, 12.9, 7.8]
+console.log(totals);
+// → [26.4, 339.25, 202.4, 528, 44.4, 120.75, 12, 1320, 98.9, 59.8]
+
+function calcAverage(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+}
+
+console.log(calcAverage(totals));; // → 275.19
