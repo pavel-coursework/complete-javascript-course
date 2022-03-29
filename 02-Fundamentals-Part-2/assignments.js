a25dbdc1a4efd59d3bae8c108d719bf4b67ac376
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-03-29 08:58:05 +0300

    Add "looping backwards and nested loops" assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -231,3 +231,31 @@ for (let i = 0; i < populations.length; i++) {
 
 console.log(percentages2);
 // → [0.08607594936708861, 18.240506329113924, 1.5822784810126582, 1.0506329113924051]
+
+
+/* LECTURE: Looping Backwards and Nested Loops
+1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
+worry if it's too difficult for you! But you can still try to figure this out anyway */
+
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+  }
+}
+// → Neighbour: Canada
+// → Neighbour: Mexico
+// → Neighbour: Spain
+// → Neighbour: Norway
+// → Neighbour: Sweden
+// → Neighbour: Russia
