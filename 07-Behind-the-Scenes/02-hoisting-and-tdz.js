bd2f4e62bddc268e4222acb23bc2a10ff0612454
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-05-22 23:41:38 +0300

    Add "hoisting and tdz" code

---
+++ 07-Behind-the-Scenes/02-hoisting-and-tdz.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Variables
+console.log(me); // → undefined
+// console.log(job);
+// console.log(year);
+
+var me = "Jonas";
+let job = "teacher";
+const year = 1991;
+
+// Functions
+
+console.log(addDecl(2, 3)); // → 5
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCard();
+var numProducts = 10;
+function deleteShoppingCard() {
+  console.log("All products deleted!"); // → All products deleted!
+}
+
+var x = 1;
+let y = 1;
+const z = 1;
+
+console.log(x === window.x); // → true
+console.log(y === window.y); // → false
+console.log(z === window.z); // → false
