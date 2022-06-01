071503ff0d083100dd0a6918d1c84eb2a3d9ec80
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-06-01 22:57:42 +0300

    First class and higher order functions

---
+++ 09-Functions/03-first-class-and-higher-order-functions.js
@@ -0,0 +1,10 @@
+/* No code here
+
+First class functions:
+• Javascript treats functions as first class citizens
+• This means that functions are simple values
+• Functions are just another "type" of object
+
+High-order functions:
+• Function that receives another function as an argument, that returns a new function, or both.
+• This is possible because of first class functions */
