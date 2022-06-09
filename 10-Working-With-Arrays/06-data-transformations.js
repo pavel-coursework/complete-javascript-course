45991e2494d4170b76cc30883082b76a6ed74b7b
Pavel Georgiev <iPavelGeorgiev@outlook.com>
2022-06-09 23:42:56 +0300

    Notes on data transformations

---
+++ 10-Working-With-Arrays/06-data-transformations.js
@@ -0,0 +1,11 @@
+/* Data Transformations with Map, Filter and Reduce
+Map:
+- current * 2
+- Multiplies every single element by 2, then create a new array
+- Returns a new array containing the results of applying an operation on all original array elements
+Filter:
+- current > 2
+- Returns a new array containing the array elements that passed a specified test condition
+Reduce:
+- boil all array elements down to one single value
+- needs accumulator variable, it will absorbs the array. */
