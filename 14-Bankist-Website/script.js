7def72a067d8c845418fba2724a94394e88d07f8
Pavel Georgiev <55008190+iPavelGeorgiev@users.noreply.github.com>
2022-09-09 12:32:40 +0300

    Implement tabbed component functionality

--- 14-Bankist-Website/script.js
+++ 14-Bankist-Website/script.js
@@ -83,3 +83,25 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     section?.scrollIntoView({ behavior: "smooth" });
   }
 });
+
+///////////////////////////////////////
+// Tabbed component
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
