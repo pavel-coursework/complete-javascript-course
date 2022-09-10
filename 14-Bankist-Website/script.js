fc8cdb455acb531db162698744396c94461f9f8a
Pavel Georgiev <55008190+iPavelGeorgiev@users.noreply.github.com>
2022-09-10 11:50:25 +0300

    Implement carousel slider

--- 14-Bankist-Website/script.js
+++ 14-Bankist-Website/script.js
@@ -197,3 +197,79 @@ const loadImg = function (entries, observer) {
 const imgObserver = new IntersectionObserver(loadImg, { rootMargin: "200px" });
 
 imgTargets.forEach((img) => imgObserver.observe(img));
+
+///////////////////////////////////////
+// Slider
+
+const slider = function () {
+  // Selectors
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+    activateDot(slide);
+  };
+
+  const nextSlide = function () {
+    curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
+    goToSlide(curSlide);
+  };
+
+  const prevSlide = function () {
+    curSlide === 0 ? (curSlide = maxSlide - 1) : curSlide--;
+    goToSlide(curSlide);
+  };
+
+  const init = function () {
+    createDots();
+    activateDot(0);
+
+    goToSlide(0);
+  };
+
+  init();
+
+  // Event Listeners
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") nextSlide();
+    if (e.key === "ArrowLeft") prevSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot"))
+      goToSlide(e.target.dataset.slide);
+  });
+};
+
+slider();
