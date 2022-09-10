63cd3ca7c5f2e0d2ed7901adb60f09ceda2f1aa2
Pavel Georgiev <55008190+iPavelGeorgiev@users.noreply.github.com>
2022-09-10 11:42:44 +0300

    Implement reveal sections effect

--- 14-Bankist-Website/script.js
+++ 14-Bankist-Website/script.js
@@ -153,3 +153,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Reveal sections effect
+const allSections = document.querySelectorAll(".section");
+
+const revealSections = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSections, {
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
